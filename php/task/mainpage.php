5ab9134abc8e21d149c857b45cf5cb388a6f513f
heatnan <964465194@qq.com>
2016-10-13 01:47:06 +0800

    纠正数据库中的错误字段

--- php/task/mainpage.php
+++ php/task/mainpage.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		 <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
 		 <link rel="stylesheet" type="text/css" href="../../css/style.css" />
-		 
 		 <style>
 			a {
 				color:black;
