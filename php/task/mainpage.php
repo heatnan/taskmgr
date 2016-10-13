121baec711fe5068714d21b74d0efb8b822ac3b4
heatnan <964465194@qq.com>
2016-10-14 03:11:54 +0800

    优化界面
    1 登陆ajax校验
    2 美化界面

--- php/task/mainpage.php
+++ php/task/mainpage.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ï»¿<!DOCTYPE html>
 <html>
 	<head>
 		 <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
@@ -7,15 +7,61 @@
 			a {
 				color:black;
 			}
+			#div_main{
+				margin-top:-300px;
+				margin-left:-500px;
+				width:1040px;
+				height:700px;
+				position:absolute;
+				top:50%;
+				left:50%;
+			}
+			.div_row{
+				margin:20px 20px;
+				width:1000px;
+				height:260px;	
+			}
+			.div_left{
+				background:white;
+				margin-left:100px;
+				margin-top:40px;
+				width:300px;
+				height:200px;
+				float:left;
+				border:2px inset gray;
+				padding:3px 3px 3px 3px;
+			}
+			.div_right{
+				background:white;
+				margin-right:100px;
+				margin-top:40px;
+				width:300px;
+				height:200px;
+				float:right;
+				border:2px outset gray;
+				padding:3px 3px 3px 3px;
+			}
 		 </style>
 	</head>
 	
-	<body class="bg_main">
-		<h1>Welcome!</h1>
-		
-		<div>
-			<a href="../../newtask.html">´´½¨ÐÂµÄÊÂÎñ</a>
-			<a href="tasklist.php">²é¿´ÊÂÎñ</a>
+	<body class="bg_main">	
+		<div id="div_main">
+			<div class="div_row">
+				<div id="div_01" class="div_left">
+					<a href="../../newtask.html"><img src="../../resource/pic/newtask01.jpg" width="300px" height="200px"></a>
+				</div>
+				<div id="div_02" class="div_right">
+					<a href="tasklist.php"><img src="../../resource/pic/book01.jpg" width="300px" height="200px"></a>
+				</div>
+			</div>
+			<div class="div_row">
+				<div id="div_03" class="div_left">
+					<a href="../../newtask.html"><img src="../../resource/pic/sports01.jpg" width="300px" height="200px"></a>
+				</div>
+				<div id="div_04" class="div_right">
+					<a href="tasklist.php"><img src="../../resource/pic/love01.jpg" width="300px" height="200px"></a>
+				</div>
+			</div>
 		</div>
 		
 	</body>
